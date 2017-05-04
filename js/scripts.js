d9fac30231e4fc62d802e711be367dcc4b00da66
eileen <eileen@eileen.com>
2017-05-04 15:45:42 +0200

    new

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+var withMenuItemsClass = document.getElementsByClassName('button'); 
+	for (var i = 0; i < withMenuItemsClass.length; i++) {
+	console.log(withMenuItemsClass.innerText);
+}
+/*
+for (var i = 0; i < withMenuItemsClass.length; i++) {
+	var innerWithMenuItemsClass = withMenuItemsClass.innerText;
+	console.log(innerWithMenuItemsClass);
+}
+*/
